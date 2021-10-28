ef4b937cd07406b770a0e02810c4e81d260a8d34
jjsasha63 <jjsasha63@gmail.com>
2021-10-28 17:14:24 +0200

    temp

--- src/main/java/com/red/Main.java
+++ src/main/java/com/red/Main.java
@@ -1,75 +1,57 @@
 package com.red;
 
 import opennlp.tools.doccat.*;
-import opennlp.tools.lemmatizer.LemmatizerME;
-import opennlp.tools.lemmatizer.LemmatizerModel;
-import opennlp.tools.postag.POSModel;
-import opennlp.tools.postag.POSTaggerME;
-import opennlp.tools.sentdetect.SentenceDetectorME;
-import opennlp.tools.sentdetect.SentenceModel;
-import opennlp.tools.tokenize.TokenizerME;
-import opennlp.tools.tokenize.TokenizerModel;
-import opennlp.tools.util.*;
-import opennlp.tools.util.model.ModelUtil;
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
 
-    private static DoccatModel model_train() throws IOException{
-        DoccatFactory factory = new DoccatFactory(new FeatureGenerator[]{new BagOfWordsFeatureGenerator()});
-        InputStreamFactory inputStreamFactory = new MarkableFileInputStreamFactory(new File("src/categorizer.txt"));
-        ObjectStream<String> stringObjectStream = new PlainTextByLineStream(inputStreamFactory, StandardCharsets.UTF_8);
-        ObjectStream<DocumentSample> sampleObjectStream = new DocumentSampleStream(stringObjectStream);
-        TrainingParameters trainingParameters = ModelUtil.createDefaultTrainingParameters();
-        trainingParameters.put(TrainingParameters.CUTOFF_PARAM,0);
-        DoccatModel doccatModel =DocumentCategorizerME.train("en",sampleObjectStream,trainingParameters,factory);
-        return doccatModel;
+    private static Map<String, String> questionAnswer = new HashMap<>();
+
+    static {
+        questionAnswer.put("greeting_casual", "greeting");
+        questionAnswer.put("greeting_polite", "greetings2");
+        questionAnswer.put("describe", "descr");
+        questionAnswer.put("start", "start");
+        questionAnswer.put("complete", "end");
+        questionAnswer.put("question","quest");
+        questionAnswer.put("continue", "cont");
+        questionAnswer.put("answ_positive", "answ_pos");
+        questionAnswer.put("answ_negative", "answ_neg");
     }
 
+    private static void chat(){
 
-    private static String[] breaks(String val) throws IOException{
-        try (InputStream inputStream = new FileInputStream("en-sent.bin")) {
-            SentenceDetectorME sentenceDetectorME = new SentenceDetectorME(new SentenceModel(inputStream));
-            String[] t_lines = sentenceDetectorME.sentDetect(val);
-            return t_lines;
+    }
+    public static void main(String[] args) throws IOException {
+        Model model = new Model();
+        boolean end = false;
+        String user, answ, category;
+        String[] text, tk, pos, lemma;
+        Scanner scanner = new Scanner(System.in);
+        DoccatModel doccatModel = model.model_train();
+        for(;;){
+            System.out.println("You: ");
+            user = scanner.nextLine();
+            text = model.breaks(user);
+            answ = "";
+            for (String i : text) {
+                tk = model.token(i);
+                pos = model.pos(tk);
+                lemma = model.lemma(pos, tk);
+                category = model.category(doccatModel, lemma);
+                answ = answ + " " + questionAnswer.get(category);
+                    if ("complete".equals(category)) {
+                        end = true;
+                    }
+            }
+        System.out.println("Bot: " + answ);
+        if (end) {
+            break;
         }
-    }
-
-    private static String[] lemma(String[] pos,String[] tk)throws  IOException{
-        try(InputStream inputStream = new FileInputStream("en-lemmatizer.bin")) {
-            LemmatizerME lemmatizerME =new LemmatizerME(new LemmatizerModel(inputStream));
-            String[] lemma_tk =lemmatizerME.lemmatize(tk,pos);
-            return lemma_tk;
         }
-    }
-
-
-    private static String[] pos(String[] tk)throws IOException{
-        try(InputStream inputStream = new FileInputStream("en-pos-maxent.bin")){
-            POSTaggerME posTaggerME = new POSTaggerME(new POSModel(inputStream));
-            String[] pos_tk = posTaggerME.tag(tk);
-            return pos_tk;
-        }
-    }
-
-    private static String[] token(String text)throws IOException{
-        try(InputStream inputStream = new FileInputStream("en-token.bin")){
-            TokenizerME tokenizerME = new TokenizerME(new TokenizerModel(inputStream));
-            String[] tk = tokenizerME.tokenize(text);
-            return tk;
-        }
-    }
-
-    private static String category(DoccatModel doccatModel,String[] tk){
-        DocumentCategorizerME documentCategorizerME = new DocumentCategorizerME(doccatModel);
-        double[] out = documentCategorizerME.categorize(tk);
-        String category = documentCategorizerME.getBestCategory(out);
-        return category;
-    }
-
-    public static void main(String[] args) {
-
     }
 }
---
+++ src/main/java/com/red/Model.java
@@ -0,0 +1,75 @@
+package com.red;
+
+import opennlp.tools.doccat.*;
+import opennlp.tools.lemmatizer.LemmatizerME;
+import opennlp.tools.lemmatizer.LemmatizerModel;
+import opennlp.tools.postag.POSModel;
+import opennlp.tools.postag.POSTaggerME;
+import opennlp.tools.sentdetect.SentenceDetectorME;
+import opennlp.tools.sentdetect.SentenceModel;
+import opennlp.tools.tokenize.TokenizerME;
+import opennlp.tools.tokenize.TokenizerModel;
+import opennlp.tools.util.*;
+import opennlp.tools.util.model.ModelUtil;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+public class Model {
+
+    static DoccatModel model_train() throws IOException {
+        DoccatFactory factory = new DoccatFactory(new FeatureGenerator[]{new BagOfWordsFeatureGenerator()});
+        InputStreamFactory inputStreamFactory = new MarkableFileInputStreamFactory(new File("src/categorizer1.txt"));
+        ObjectStream<String> stringObjectStream = new PlainTextByLineStream(inputStreamFactory, StandardCharsets.UTF_8);
+        ObjectStream<DocumentSample> sampleObjectStream = new DocumentSampleStream(stringObjectStream);
+        TrainingParameters trainingParameters = ModelUtil.createDefaultTrainingParameters();
+        trainingParameters.put(TrainingParameters.CUTOFF_PARAM,0);
+        DoccatModel doccatModel =DocumentCategorizerME.train("en",sampleObjectStream,trainingParameters,factory);
+        return doccatModel;
+    }
+
+
+    static String[] breaks(String val) throws IOException{
+        try (InputStream inputStream = new FileInputStream("en-sent.bin")) {
+            SentenceDetectorME sentenceDetectorME = new SentenceDetectorME(new SentenceModel(inputStream));
+            String[] t_lines = sentenceDetectorME.sentDetect(val);
+            return t_lines;
+        }
+    }
+
+    static String[] lemma(String[] pos, String[] tk)throws  IOException{
+        try(InputStream inputStream = new FileInputStream("en-lemmatizer.bin")) {
+            LemmatizerME lemmatizerME =new LemmatizerME(new LemmatizerModel(inputStream));
+            String[] lemma_tk =lemmatizerME.lemmatize(tk,pos);
+            return lemma_tk;
+        }
+    }
+
+
+    static String[] pos(String[] tk)throws IOException{
+        try(InputStream inputStream = new FileInputStream("en-pos-maxent.bin")){
+            POSTaggerME posTaggerME = new POSTaggerME(new POSModel(inputStream));
+            String[] pos_tk = posTaggerME.tag(tk);
+            return pos_tk;
+        }
+    }
+
+    static String[] token(String text)throws IOException{
+        try(InputStream inputStream = new FileInputStream("en-token.bin")){
+            TokenizerME tokenizerME = new TokenizerME(new TokenizerModel(inputStream));
+            String[] tk = tokenizerME.tokenize(text);
+            return tk;
+        }
+    }
+
+    static String category(DoccatModel doccatModel, String[] tk){
+        DocumentCategorizerME documentCategorizerME = new DocumentCategorizerME(doccatModel);
+        double[] out = documentCategorizerME.categorize(tk);
+        String category = documentCategorizerME.getBestCategory(out);
+        return category;
+    }
+
+}
